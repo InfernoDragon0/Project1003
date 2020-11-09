5f0daab853accc503a90a8a13e09729dd08e25d6
junjie98 <64370041+junjie98@users.noreply.github.com>
2020-11-09 18:53:47 +0800

    Watch with BT Functionality Stripped

---
+++ WatchNoBT/WatchNoBT.ino
@@ -0,0 +1,132 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <TinyScreen.h>
+
+#define menu_debug_print true
+
+//#define ARDUINO_ARCH_SAMD
+
+#if defined (ARDUINO_ARCH_AVR)
+TinyScreen display = TinyScreen(TinyScreenDefault);
+#define SerialMonitorInterface Serial
+#include <TimeLib.h>
+
+#elif defined(ARDUINO_ARCH_SAMD)
+TinyScreen display = TinyScreen(TinyScreenDefault);
+#define SerialMonitorInterface SerialUSB
+#include <RTCZero.h>
+#include <time.h>
+RTCZero RTCZ;
+uint32_t startTime = 0;
+uint32_t sleepTime = 0;
+unsigned long millisOffsetCount = 0;
+
+void wakeHandler() {
+  if (sleepTime) {
+    millisOffsetCount += (RTCZ.getEpoch() - sleepTime);
+    sleepTime = 0;
+  }
+}
+#endif
+const FONT_INFO& font10pt = thinPixel7_10ptFontInfo;
+const FONT_INFO& font22pt = liberationSansNarrow_22ptFontInfo;
+
+uint32_t newtime = 0;
+
+uint8_t defaultFontColor = TS_8b_White;
+uint8_t defaultFontBG = TS_8b_Black;
+uint8_t inactiveFontColor = TS_8b_Gray;
+uint8_t inactiveFontBG = TS_8b_Black;
+
+uint8_t topBarHeight = 10;
+uint8_t timeY = 14;
+uint8_t menuTextY[4] = {12, 25, 38, 51};
+
+unsigned long batteryUpdateInterval = 10000;
+unsigned long lastBatteryUpdate = 0;
+
+unsigned long sleepTimer = 0;
+int sleepTimeout = 25;
+
+uint8_t rewriteTime = true;
+
+uint8_t displayOn = 0;
+uint8_t buttonReleased = 1;
+uint8_t rewriteMenu = false;
+unsigned long mainDisplayUpdateInterval = 300;
+unsigned long lastMainDisplayUpdate = 0;
+
+int brightness = 3;
+uint8_t lastSetBrightness = 100;
+
+void setup(void) {
+  #if defined (ARDUINO_ARCH_AVR)
+  for (int i = 0; i < 20; i++) {
+    pinMode(i, INPUT_PULLUP);
+  }
+  setTime(18, 34, 10, 9, 11, 20); //h, m, s, d, m, y
+  #elif defined(ARDUINO_ARCH_SAMD)
+  RTCZ.begin();
+  RTCZ.setTime(18, 34, 10);//h,m,s
+  RTCZ.setDate(9, 11, 20);//d,m,y
+  #endif
+  // put your setup code here, to run once:
+  Wire.begin();
+  SerialMonitorInterface.begin(115200);
+  display.begin();
+  display.on();
+  display.setFlip(true);
+  display.setBrightness(10);
+  display.setFont(font10pt);
+  display.fontColor(defaultFontColor, defaultFontBG);
+  initHomeScreen();
+  requestScreenOn();
+  delay(100);
+}
+
+uint32_t millisOffset() {
+#if defined (ARDUINO_ARCH_AVR)
+  return millis();
+#elif defined(ARDUINO_ARCH_SAMD)
+  return (millisOffsetCount * 1000ul) + millis();
+#endif
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (displayOn && (millisOffset() > mainDisplayUpdateInterval + lastMainDisplayUpdate)) {
+    updateMainDisplay();
+  }
+  if (millisOffset() > sleepTimer + ((unsigned long)sleepTimeout * 1000ul)) {
+    if (displayOn) {
+      displayOn = 0;
+      display.off();
+    }
+  }
+  checkButtons();
+  displayBattery();
+}
+
+int requestScreenOn() {
+  sleepTimer = millisOffset();
+  if (!displayOn) {
+    displayOn = 1;
+    updateMainDisplay();
+    display.on();
+    return 1;
+  }
+  return 0;
+}
+
+void checkButtons() {
+  byte buttons = display.getButtons();
+  if (buttonReleased && buttons) {
+    if (displayOn)
+      buttonPress(buttons);
+    requestScreenOn();
+    buttonReleased = 0;
+  }
+  if (!buttonReleased && !(buttons & 0x0F)) {
+    buttonReleased = 1;
+  }
+}
