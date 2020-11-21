cb4b6ef3e49072bceee83614ec58de3c402fddc1
junjie98 <64370041+junjie98@users.noreply.github.com>
2020-11-21 23:59:17 +0800

    Update games.ino

--- SmartWatchAndroid/games.ino
+++ SmartWatchAndroid/games.ino
@@ -15,6 +15,27 @@ unsigned char flappyBirdBitmap[204] = {
   TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black
 };
 
+unsigned char ghost[361] = {
+  TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,
+  TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,
+  TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,
+  TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,
+  TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,TS_8b_Black,TS_8b_Black,
+  TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_Red,TS_8b_Black,TS_8b_Black,
+  TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_Red,TS_8b_Black,TS_8b_Black,
+  TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_Black,TS_8b_Black,
+  TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Black,
+  TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_White,TS_8b_White,TS_8b_White,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,
+  TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,
+  TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,
+  TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,
+  TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,
+  TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,
+  TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,
+  TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black,
+  TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Black,TS_8b_Red,TS_8b_Red,TS_8b_Red,TS_8b_Black
+};
+
 // Modifier icons
 unsigned char heart[40] = {
   TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black, TS_8b_Black,
@@ -135,17 +156,17 @@ void chkDead() {
 }
 
 void drawBitmap() {
-  //set a background that matches
-  //display.drawRect(0, 12, 96, 64,TSRectangleFilled,TS_8b_Blue);
-  //let's set up for a bitmap at (40,30) that is 17 pixels wide and 12 pixels tall:
-  //setX(x start, x end);//set OLED RAM to x start, wrap around at x end
   display.setX(40, 40 + 17 - 1);
-  //setY(y start, y end);//set OLED RAM to y start, wrap around at y end
   display.setY(30, 30 + 12 - 1);
-  //now start a data transfer
   display.startData();
-  //writeBuffer(buffer,count);//optimized write of a large buffer of 8 bit data.
   display.writeBuffer(flappyBirdBitmap, 17 * 12);
+  display.endTransfer();
+}
+void drawGhost() {
+  display.setX(40, 40 + 19 - 1);
+  display.setY(25, 25 + 19 - 1);
+  display.startData();
+  display.writeBuffer(ghost, 19 * 19);
   display.endTransfer();
 }
 void drawHeart() {
@@ -211,7 +232,7 @@ void loop1() {
   RTP();
   updateGold();
   display.clearWindow(0, 45, 96, 64);
-  drawBitmap();
+  drawGhost();
   display.setCursor(0, 52);
   display.print("Meowelcome Back!");
   drawHeart(); //remove before prod
