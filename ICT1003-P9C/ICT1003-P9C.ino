555388b7ad3f304bf09795349e74bb441a37b782
junjie98 <64370041+junjie98@users.noreply.github.com>
2020-11-24 00:28:31 +0800

    Almost There Buddy

--- ICT1003-P9C/ICT1003-P9C.ino
+++ ICT1003-P9C/ICT1003-P9C.ino
@@ -1,19 +1,148 @@
-#include <Wire.h>
 #include <SPI.h>
 #include <TinyScreen.h>
+#include <Wire.h>
 
+#define menu_debug_print true
+
+#if defined (ARDUINO_ARCH_AVR)
 TinyScreen display = TinyScreen(TinyScreenDefault);
+#define SerialMonitorInterface Serial
+#include <TimeLib.h>
 
-void setup() {
+#elif defined(ARDUINO_ARCH_SAMD)
+TinyScreen display = TinyScreen(TinyScreenDefault);
+#define SerialMonitorInterface SerialUSB
+#include <RTCZero.h>
+#include <time.h>
+RTCZero RTCZ;
+uint32_t startTime = 0;
+uint32_t sleepTime = 0;
+unsigned long millisOffsetCount = 0;
+
+void wakeHandler() {
+  if (sleepTime) {
+    millisOffsetCount += (RTCZ.getEpoch() - sleepTime);
+    sleepTime = 0;
+  }
+}
+#endif
+
+uint8_t defaultFontColor = TS_8b_White;
+uint8_t defaultFontBG = TS_8b_Black;
+uint8_t inactiveFontColor = TS_8b_Gray;
+uint8_t inactiveFontBG = TS_8b_Black;
+
+uint8_t topBarHeight = 10;
+uint8_t timeY = 14;
+uint8_t menuTextY[4] = {12, 25, 38, 51};
+
+unsigned long batteryUpdateInterval = 10000;
+unsigned long lastBatteryUpdate = 0;
+
+unsigned long sleepTimer = 0;
+int sleepTimeout = 5;
+
+uint8_t rewriteTime = true;
+
+uint8_t displayOn = 0;
+uint8_t buttonReleased = 1;
+uint8_t rewriteMenu = false;
+unsigned long mainDisplayUpdateInterval = 300;
+unsigned long lastMainDisplayUpdate = 0;
+
+int brightness = 5;
+uint8_t lastSetBrightness = 100;
+
+const FONT_INFO& font10pt = thinPixel7_10ptFontInfo;
+const FONT_INFO& font22pt = liberationSansNarrow_22ptFontInfo;
+
+void setup(void) {
+  #if defined (ARDUINO_ARCH_AVR)
+  for (int i = 0; i < 20; i++) {
+    pinMode(i, INPUT_PULLUP);
+  }
+  setTime(22, 10, 10, 24, 11, 20); //h, m, s, d, m, y
+  #elif defined(ARDUINO_ARCH_SAMD)
+  RTCZ.begin();
+  RTCZ.setTime(23, 00, 00);//h,m,s
+  RTCZ.setDate(24, 11, 20);//d,m,y
+  #endif
   // put your setup code here, to run once:
   Wire.begin();
+  SerialMonitorInterface.begin(115200);
   display.begin();
   display.on();
   display.setFlip(true);
-  display.setBrightness(10);  
+  display.setBrightness(10);
+  display.setFont(font10pt);
+  display.fontColor(defaultFontColor, defaultFontBG);
+  initHomeScreen();
+  requestScreenOn();
+  delay(100);
+}
+
+uint32_t millisOffset() {
+#if defined (ARDUINO_ARCH_AVR)
+  return millis();
+#elif defined(ARDUINO_ARCH_SAMD)
+  return (millisOffsetCount * 1000ul) + millis();
+#endif
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (displayOn && (millisOffset() > mainDisplayUpdateInterval + lastMainDisplayUpdate)) {
+    updateMainDisplay();
+  }
+  if (millisOffset() > sleepTimer + ((unsigned long)sleepTimeout * 1000ul)) {
+    if (displayOn) {
+      displayOn = 0;
+      display.off();
+    }
+  }
+  checkButtons();
+  displayBattery();
+}
 
+int requestScreenOn() {
+  sleepTimer = millisOffset();
+  if (!displayOn) {
+    displayOn = 1;
+    updateMainDisplay();
+    display.on();
+    return 1;
+  }
+  return 0;
+}
+
+void checkButtons() {
+  byte buttons = display.getButtons();
+  if (buttonReleased && buttons) {
+    if (displayOn)
+      buttonPress(buttons);
+    requestScreenOn();
+    buttonReleased = 0;
+  }
+  if (!buttonReleased && !(buttons & 0x0F)) {
+    buttonReleased = 1;
+  }
+}
+
+// Real-Time tracking BETA
+uint32_t astartLTime = 0;
+uint32_t aendLTime = 0;
+uint8_t deduct = 0;
+uint16_t atimeDiff = 0;
+uint32_t atimeElapsed = 0;
+
+uint16_t penalty(){
+  astartLTime = millis();
+  aendLTime = millis() + 1;
+  atimeDiff = aendLTime - astartLTime;
+  atimeElapsed += atimeDiff;
+  if(atimeElapsed >= 864000){
+    atimeElapsed = 0;
+    deduct += 1; // use this value for gold gen
+  }
+  return deduct;
 }
